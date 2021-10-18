46ff5b5112b31bfc64693ab2451bf65da8f853d5
Washington Junior <me@washingtonj.dev>
2021-10-17 22:52:23 -0300

    feat: Alert if you don't set a quantity on purchase

--- src/views/Content/ContentPurchase.spec.tsx
+++ src/views/Content/ContentPurchase.spec.tsx
@@ -4,6 +4,7 @@ import ContentPurchase from './ContentPurchase'
 describe('Content - Purchase', () => {
   let plusButton: HTMLButtonElement;
   let minusButton: HTMLButtonElement;
+  let purchaseButton: HTMLButtonElement;
   let counter: HTMLParagraphElement;
   
   beforeEach(() => {
@@ -11,6 +12,7 @@ describe('Content - Purchase', () => {
 
     plusButton = screen.getByTestId('plus') as HTMLButtonElement
     minusButton = screen.getByTestId('minus') as HTMLButtonElement
+    purchaseButton = screen.getByTestId('purchase') as HTMLButtonElement
     counter = screen.getByTestId('counter') as HTMLParagraphElement
   })
 
@@ -25,5 +27,11 @@ describe('Content - Purchase', () => {
     fireEvent.click(plusButton)
     fireEvent.click(minusButton)
     expect(counter.innerHTML).toBe("0")
+  })
+
+  it('Should advise if trying to add to cart without setting an amount', () => {
+    fireEvent.click(purchaseButton)
+    const alert = screen.getByText(/Insert a quantity would you like to purchase/i)
+    expect(alert).toBeInTheDocument()
   })
 })
--- src/views/Content/ContentPurchase.tsx
+++ src/views/Content/ContentPurchase.tsx
@@ -1,10 +1,11 @@
-import { Flex, Button, Text } from '@chakra-ui/react'
+import { Flex, Button, Text, Tooltip } from '@chakra-ui/react'
 import { Minus, Plus, Cart } from 'assets/icons'
 import { useState } from 'react'
 import * as Styles from './ContentPurchase.styles'
 
 const ContentPurchase = () => {
   const [quantity, setQuantity] = useState(0)
+  const [emptyAlert, setEmptyAlert] = useState(false)
 
   function increase() {
     setQuantity(prev => prev + 1)
@@ -14,19 +15,37 @@ const ContentPurchase = () => {
     if (quantity > 0) setQuantity(prev => prev - 1)
   }
 
+  function purchase() {
+    if (quantity === 0) setEmptyAlert(true)
+  }
 
   return (
     <>
-      <Flex { ...Styles.QuantityContainer }>
-        <Button data-testid="minus" onClick={() => decrease()}>
-          <Minus />
-        </Button>
-        <Text data-testid="counter" fontWeight="bold">{ quantity }</Text>
-        <Button data-testid="plus" onClick={() => increase()}>
-          <Plus />
-        </Button>
-      </Flex>
-      <Button {...Styles.Button} leftIcon={<Cart width="22" height="20" fill='white' />}>
+      <Tooltip
+        label="Insert a quantity would you like to purchase"
+        placement='top'
+        isDisabled={!emptyAlert}
+        borderRadius='md'
+        background="orange.400"
+        isOpen={emptyAlert}
+      >
+        <Flex 
+          {...Styles.QuantityContainer} onMouseMove={() => setEmptyAlert(false)}>
+          <Button data-testid="minus" onClick={() => decrease()}>
+            <Minus />
+          </Button>
+          <Text data-testid="counter" fontWeight="bold">{quantity}</Text>
+          <Button data-testid="plus" onClick={() => increase()}>
+            <Plus />
+          </Button>
+        </Flex>
+      </Tooltip>
+      <Button 
+        {...Styles.Button} 
+        data-testid="purchase"
+        leftIcon={<Cart width="22" height="20" fill='white' />}
+        onClick={() => purchase()}
+      >
         Add to cart
       </Button>
     </>
